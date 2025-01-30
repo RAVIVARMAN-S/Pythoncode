cdf5e8576626738fa895895dbe8973eba8d0dcbf
RAVIVARMAN-S <ravivarman91867@gmail.com>
2025-01-30 18:36:46 +0530

    Add files via upload

---
+++ repeated.py
@@ -0,0 +1,3 @@
+a=int(input("enter the integer:"))
+for i in range(a):
+    print("ALL IS WELL")
---
+++ rightangle.py
@@ -0,0 +1,7 @@
+a=int(input())
+
+for i in range(1,a+1):
+    b=""
+    for j in range(1,i+1):
+        b=b+str(j)+" "
+    print(b)
