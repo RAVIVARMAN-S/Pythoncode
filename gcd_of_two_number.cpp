101724784b8df8e2e1e4f6b6ff98f2d1dcf45c46
RAVIVARMAN-S <ravivarman91867@gmail.com>
2025-02-16 23:28:01 +0530

    Create gcd_of_two_number.cpp

---
+++ gcd_of_two_number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int num1,num2;
+   cout<<"enter the valur:";
+   cin>>num1>>num2;
+   while(num1!=0 && num2!=0) // checks value of num1 and num2 are 0 until the condition false
+   {
+    num1=num1%10; //store the remainder of num1
+    num2=num2%10; //store the remainder of num2
+  
+
+   }
+   if(num1==0){cout<<num2;} //if num1 is '0' means print value of num2
+   else{cout<<num1;} //if num2 is '0' means print value of num1
+   
+}
